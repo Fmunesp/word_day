b6a3ccefa66e82450bb350e04c293fc8f9904448
Fran <franciscojaviermontielnoguera@gmail.com>
2025-02-09 11:42:11 +0100

    add functionality and structure

---
+++ scripts/main.js
@@ -0,0 +1,18 @@
+const crearTablero = (mainContainer) => {
+    // rellenamos el tablero
+    for (let i = 0; i < 6; i++) {
+        mainContainer.innerHTML += `<div id="container_row_${i}" class="row">`;
+        for (let j = 0; j < 5; j++) {
+            mainContainer.innerHTML += `<input type="text" id="input_${i}_${j}" maxlength="1">`;
+        }
+        mainContainer.innerHTML += `</div>`;
+    }
+}
+
+// Una vez cargado el DOM, ejecutamos la función principal
+document.addEventListener('DOMContentLoaded', () => {
+    
+    const mainContainer = document.getElementById('main_container');
+    crearTablero(mainContainer);
+
+});
